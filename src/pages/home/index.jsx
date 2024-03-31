29948abb3c29fcb3f427bdd61d89ae08be95b2a1
Prabu Addin Almuhasibi <prabualmuhasibi@gmail.com>
2024-03-31 20:27:07 +0700

    feat: about section

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,14 @@
+import { useTypewriter } from "react-simple-typewriter";
 export default function HomePage() {
+  const [text] = useTypewriter({
+    words: [
+      "Stepping into the tech world as a fresh graduate from Software Engineering, armed with passion, knowledge, and a hunger to innovate. Ready to code my way to success! 👨🏻‍💻",
+    ],
+    loop: {},
+    typeSpeed: 120,
+    deleteSpeed: 100,
+  });
+
   return (
     <>
       <div
@@ -11,15 +21,29 @@ export default function HomePage() {
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">Hello everyone</h1>
-            <p className="mb-5">
-              "Stepping into the tech world as a fresh graduate from Software
-              Engineering, armed with passion, knowledge, and a hunger to
-              innovate. Ready to code my way to success! 👨🏻‍💻"
-            </p>
+            <p className="mb-5">{text}</p>
           </div>
         </div>
       </div>
-      <hr />
+      <div>
+        <h1 className="font-bold text-lg text-center mt-5">About Me</h1>
+        <div className="divider w-20 m-auto"></div>
+        <p className="px-5 text-center">
+          Currently, I have completed my studies from the diploma of application
+          software engineering at Telkom University and am continuing my
+          undergraduate extension education at Binus University and also I am
+          currently attending the Job Connector Full Stack Web Development
+          program at Purwadhika. I previously had an internship experience as a
+          game programmer at PT Konsep Allgoritma Indonesia and developed a
+          simulation game. I have an interest in software development, but
+          currently focus more on web development. To support this, I study
+          HTML, CSS, Javascript, as well as frameworks or libraries that are
+          widely used by the industry today. However, it is possible that I will
+          take a job related to AR/VR Development because I have experience in
+          developing this application when completing the final project for
+          graduation requirements from my college major.
+        </p>
+      </div>
       <h1 className="font-bold text-lg text-center mt-5">Proyek</h1>
       <div className="divider w-20 m-auto"></div>
       <ul className="timeline timeline-snap-icon px-3 max-md:timeline-compact timeline-vertical">
@@ -40,12 +64,10 @@ export default function HomePage() {
           <div className="timeline-start md:text-end mb-10">
             <time className="font-mono italic">2022</time>
             <div className="text-lg font-black">Book For You</div>
-            The Apple Macintosh—later rebranded as the Macintosh 128K—is the
-            original Apple Macintosh personal computer. It played a pivotal role
-            in establishing desktop publishing as a general office function. The
-            motherboard, a 9 in (23 cm) CRT monitor, and a floppy drive were
-            housed in a beige case with integrated carrying handle; it came with
-            a keyboard and single-button mouse.
+            It is an application to provide reviews and share experiences from
+            books that have been read by reviewers. Then in this application
+            other users can add comments and likes to the book reviews they
+            read.
           </div>
           <hr />
         </li>
@@ -67,10 +89,11 @@ export default function HomePage() {
           <div className="timeline-end mb-10">
             <time className="font-mono italic">2023</time>
             <div className="text-lg font-black">Smart Divitar</div>
-            iMac is a family of all-in-one Mac desktop computers designed and
-            built by Apple Inc. It has been the primary part of Apple's consumer
-            desktop offerings since its debut in August 1998, and has evolved
-            through seven distinct forms
+            This project was done to fulfill the graduation requirements of my
+            college major. This project is an augmented reality-based
+            application integrated with chatGPT. This application has a theme to
+            reduce the stress of its users by chatting with a three-dimensional
+            avatar designed as a conversation partner.
           </div>
           <hr />
         </li>
@@ -78,7 +101,7 @@ export default function HomePage() {
       <h1 className="font-bold text-lg text-center mt-5">Service</h1>
       <div className="divider w-20 m-auto"></div>
       <div className="flex flex-col items-center justify-center gap-7 p-10 lg:flex-row">
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card w-96 bg-base-100 shadow-lg hover:shadow-black">
           <figure className="px-10 pt-10">
             <img
               src="https://img.freepik.com/free-vector/hand-drawn-web-developers_23-2148819604.jpg"
@@ -88,13 +111,9 @@ export default function HomePage() {
           </figure>
           <div className="card-body items-center text-center">
             <h2 className="card-title">Website Development</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions">
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
           </div>
         </div>
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card w-96 bg-base-100 shadow-lg hover:shadow-black">
           <figure className="px-10 pt-10">
             <img
               src="https://img.freepik.com/free-vector/app-development-banner_33099-1720.jpg?size=626&ext=jpg&ga=GA1.1.735520172.1711411200&semt=ais"
@@ -104,13 +123,9 @@ export default function HomePage() {
           </figure>
           <div className="card-body items-center text-center">
             <h2 className="card-title">Mobile Development</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions">
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
           </div>
         </div>
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card w-96 bg-base-100 shadow-lg hover:shadow-black">
           <figure className="px-10 pt-10">
             <img
               src="https://wallpapers.com/images/hd/game-development-2000-x-1333-wallpaper-i3eyywjsfmou1hm5.jpg"
@@ -120,10 +135,6 @@ export default function HomePage() {
           </figure>
           <div className="card-body items-center text-center">
             <h2 className="card-title">Game Development</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions">
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
           </div>
         </div>
       </div>
